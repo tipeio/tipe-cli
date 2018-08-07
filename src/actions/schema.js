545673712a5b8d356144242c6ad945107b8f0590
PatrickJS <github@gdi2290.com>
2018-08-07 10:45:23 -0700

    feat: schema hello world

---
+++ src/actions/schema.js
@@ -0,0 +1,17 @@
+const fs = require('fs.promised');
+const path = require('path');
+const program = require('commander');
+
+const {  } = require('../utilities');
+const {  } = require('../constants');
+
+program
+  .command('schema')
+  .alias('s')
+  .action(action);
+
+async function action(name) {
+  const CURRENT_DIR = process.cwd();
+
+  console.log('hello world schema')
+}
