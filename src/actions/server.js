8089e25c9a1398463fbc82bce45c5892b191b53a
Mike Moss <realmike33@gmail.com>
2018-08-07 16:42:43 -0700

    :construction: WIP

--- src/actions/server.js
+++ src/actions/server.js
@@ -6,11 +6,39 @@ const program = require('commander')
 // const { store } = require('../utilities')
 // const {} = require('../constants')
 
+const express = require('express');
+const { ApolloServer, gql } = require('apollo-server-express');
+
+const PORT = 4000;
+
+const app = express();
+
+const typeDefs = gql`
+  type Query {
+    hello: String
+  }
+`;
+
+const resolvers = {
+  Query: {
+    hello: () => 'Hello world!'
+  },
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app });
+
 program
   .command('server') // <-- your command
+  .options('--port', 'Port server to run')
   .action(action)
 
+  
 async function action(name) {
+  const defaultPort = 4000
+  const PORT = program.port || defaultPort
+  app.listen({ port: PORT }, () =>
+    console.log(`🚀 Server ready at http://localhost:${PORT}`)
+  )
   // const CURRENT_DIR = process.cwd()
-  console.log('Hello World')
 }
