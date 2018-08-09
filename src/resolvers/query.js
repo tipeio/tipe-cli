f25f3e2c4be45a8762953433309a7b0198d92165
Mike Moss <realmike33@gmail.com>
2018-08-08 20:17:38 -0700

    :wrench: fixes

--- src/resolvers/query.js
+++ src/resolvers/query.js
@@ -1,11 +1,4 @@
-const { createWhereArgs } = require('./args')
 const { parseResolveInfo, simplify } = require('graphql-parse-resolve-info')
-const {
-  GraphQLNonNull,
-  GraphQLList,
-  GraphQLString,
-  GraphQLInt
-} = require('graphql')
 const genFakeContent = require('./fake')
 
 const getOne = (type, schemaTemplateData, userSchema) => {
@@ -14,8 +7,7 @@ const getOne = (type, schemaTemplateData, userSchema) => {
       const parsedInfo = parseResolveInfo(info)
       const queryInfo = simplify(parsedInfo, userSchema.getType(type.name))
       return genFakeContent(queryInfo, type.fields, schemaTemplateData)
-    },
-    type: new GraphQLNonNull(userSchema.getType(type.name))
+    }
   }
 }
 
@@ -30,28 +22,7 @@ const getMany = (type, schemaTemplateData, userSchema) => {
         genFakeContent(queryInfo, type.fields, schemaTemplateData),
         genFakeContent(queryInfo, type.fields, schemaTemplateData)
       ]
-    },
-    args: {
-      where: { type: createWhereArgs(type, userSchema) },
-      order_by: {
-        type: new GraphQLList(new GraphQLNonNull(GraphQLString)),
-        description:
-          'List of fields to order the results by. Defaults to ["+updated_at"]',
-        defaultValue: ['+updated_at']
-      },
-      limit: {
-        type: GraphQLInt,
-        description: 'Maximum number of results to return. Defaults to 50',
-        defaultValue: 50
-      },
-      skip: {
-        type: GraphQLInt,
-        description:
-          'For pagination, how many items to skip to continue the next page',
-        defaultValue: 0
-      }
-    },
-    type: new GraphQLNonNull(new GraphQLList(userSchema.getType(type.name)))
+    }
   }
 }
 
@@ -61,8 +32,7 @@ const getPage = (type, schemaTemplateData, userSchema) => {
       const parsedInfo = parseResolveInfo(info)
       const queryInfo = simplify(parsedInfo, userSchema.getType(type.name))
       return genFakeContent(queryInfo, type.fields, schemaTemplateData)
-    },
-    type: new GraphQLNonNull(userSchema.getType(type.name))
+    }
   }
 }
 
