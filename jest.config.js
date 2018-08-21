8163230c7b689182397e606ed4fd538b17e71c48
Mike Moss <realmike33@gmail.com>
2018-08-21 11:07:36 -0700

    refactor(tests): removed mocha and added jest config

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,9 @@ module.exports = {
   testEnvironment: 'node',
   automock: false,
   bail: true,
-  verbose: true
+  verbose: true,
+  collectCoverage: true,
+  collectCoverageFrom: ['src/**', '!**/node_modules/**'],
+  notify: true,
+  notifyMode: 'always'
 }
