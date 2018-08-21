2d6a9a515c0374b1bd3f46ad1897d981409d098a
Mike Moss <realmike33@gmail.com>
2018-08-20 18:14:03 -0700

    test(server): server command testrs

---
+++ test/commands/server.test.js
@@ -0,0 +1,28 @@
+const { expect, test } = require('@oclif/test')
+const path = require('path')
+var exec = require('child_process').exec
+
+describe('server', () => {
+  test
+    .stdout()
+    .command([
+      'server',
+      `${path.join(__dirname, './../helpers/tipe-schema.graphql')}`
+    ])
+    .it('runs server', ctx => {
+      expect(ctx.stdout).contain('Server ready at http://localhost:4000')
+    })
+
+  test
+    .stdout()
+    .command([
+      'server',
+      `${path.join(__dirname, './../helpers/tipe-schema.graphql')}`,
+      '--port',
+      3000
+    ])
+    .it('runs server --port 3000', ctx => {
+      expect(ctx.stdout).contain('Server ready at http://localhost:3000')
+      exec('lsof -i :3000')
+    })
+})
