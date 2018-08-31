61c943beb3ce996d389643e7be0591992261930a
Mike Moss <realmike33@gmail.com>
2018-08-31 13:22:18 -0700

    feat(pull): creates schema file from schema stored on tipe

--- src/commands/pull.js
+++ src/commands/pull.js
@@ -1,31 +1,46 @@
 const { Command, flags } = require('@oclif/command')
 const fetch = require('node-fetch')
-// const fs = require('fs.promised')
-const path = require('path')
+const fs = require('fs.promised')
 
-const { DEV_API_ENDPOINT } = require('.../constants')
+const { DEV_API_ENDPOINT } = require('../constants')
 
 class PullCommand extends Command {
   async run() {
     const { args } = this.parse(PullCommand)
-    // const { flags } = this.parse(PullCommand)
-    // const pathToStoreSchema = flags.path || flags.p
+    const { flags } = this.parse(PullCommand)
+    const fileName = flags.fileName
     const projectId = args.projectId
+
     const schema = await fetch(`${DEV_API_ENDPOINT}/schema/${projectId}`, {
       method: 'get',
       headers: { 'Content-Type': 'application/json' }
     })
-    this.log(schema)
+      .then(res => {
+        if (res.status === 200) {
+          return res.json()
+        }
+        console.log('Error: Schema not found based on project ID')
+      })
+      .catch(() => {
+        console.log('ERROR: Server Error!')
+      })
+
+    const error = fs.writeFileSync(`${fileName}.graphql`, schema.data)
+    if (error) {
+      this.log(error)
+      return
+    }
+    this.log('Success!')
   }
 }
 
 PullCommand.description = 'Fetch a remote project schema'
 
 PullCommand.flags = {
-  path: flags.string({
-    char: 'p',
-    description: 'path to store schema',
-    default: `${path.resolve(__dirname)}`
+  fileName: flags.string({
+    char: 'n',
+    description: 'name of new file',
+    default: 'tipe'
   })
 }
 
