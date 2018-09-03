3183ea51c1e6e1e15d622ff3a7005cc861734ff8
Mike Moss <realmike33@gmail.com>
2018-09-03 13:09:35 -0700

    feat(auth): check if user is auth before running commands. removed unused command.

--- src/commands/schema.js
+++
@@ -1,9 +0,0 @@
-const { Command } = require('@oclif/command')
-
-class SchemaCommand extends Command {
-  async run() {
-    this.log('Hellow world schema command!')
-  }
-}
-
-module.exports = SchemaCommand
