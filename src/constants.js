b4689936ea83c9ac471df700c0b8eee900615947
Mike Moss <realmike33@gmail.com>
2018-09-05 15:19:44 -0700

    chore(code): removed dead code

--- src/constants.js
+++ src/constants.js
@@ -1,12 +1,8 @@
-const path = require('path')
 const pkg = require('../package.json')
-const TEMPLATES_PATH = path.join(__dirname, '../templates')
 
 module.exports = {
   VERSION: pkg.version,
   NAME: pkg.name,
   DESCRIPTION: pkg.description,
-  TEMPLATES_PATH,
-  ACTION_PATH: path.join(__dirname, './actions'),
   DEV_API_ENDPOINT: 'http://localhost:3001'
 }
