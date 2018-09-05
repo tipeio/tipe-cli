3065c0bf24cc4c5c37aedc2bbbc08c6a7373702f
Mike Moss <realmike33@gmail.com>
2018-09-04 18:59:37 -0700

    refactor(commands): Refactored api calls to include API keys. removed args and flags.

--- src/commands/pull.js
+++ src/commands/pull.js
@@ -4,33 +4,23 @@ const { pull } = require('../api')
 
 class PullCommand extends Command {
   async run() {
-    const { args } = this.parse(PullCommand)
     const { flags } = this.parse(PullCommand)
     const fileName = flags.fileName
-    const projectId = args.projectId
 
     cli.action.start('Fetching project shcema...')
-    await pull(projectId, fileName)
+    await pull(fileName)
     cli.action.stop()
   }
 }
 
-PullCommand.description = 'Fetch a remote project schema'
+PullCommand.description = 'Fetch a remote project schema, based on API key'
 
 PullCommand.flags = {
   fileName: flags.string({
     char: 'n',
     description: 'name of new file',
-    default: 'tipe'
+    default: 'tipe.graphql'
   })
 }
-
-PullCommand.args = [
-  {
-    name: 'projectId',
-    required: true,
-    description: 'ID of the project to fetch schema'
-  }
-]
 
 module.exports = PullCommand
