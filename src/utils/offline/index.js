ff3fc96e73ce1562d334eef7216ade61f465749b
Scott Moss <willscottmoss@gmail.com>
2019-03-14 13:58:31 -0700

    fix(offline): add express middleware
    add logger
    fix cors
    add body parser

--- src/utils/offline/index.js
+++ src/utils/offline/index.js
@@ -1,11 +1,17 @@
 import express from 'express'
 import { byId, byType } from './controllers'
 import { createDB } from './db'
-
+import cors from 'cors'
+import morgan from 'morgan'
+import { json, urlencoded } from 'body-parser'
 export const createServer = shapes => {
   const app = express()
   const db = createDB(shapes)
 
+  app.use(cors())
+  app.use(morgan('dev'))
+  app.use(json())
+  app.use(urlencoded({ extended: true }))
   app.get('/document/:id', byId(shapes, db))
   app.get('/documents/:type', byType(shapes, db))
 
