462205c154aef65255cd429b57814564de10d35b
Mike Moss <realmike33@gmail.com>
2019-11-11 11:40:01 -0800

    chore(utils): removed unused code. updated schema lib

--- src/utils/templates.js
+++ src/utils/templates.js
@@ -14,14 +14,14 @@ const mapObjectToList = (o, keyField) =>
 
 export const mapTemplatesForAPI = templates =>
   mapObjectToList(templates, 'id').map(template => {
-    template.fields = mapObjectToList(template.fields, 'id').map(f => {
-      const { mock, ...all } = f
-      return all
-    })
+    // template.fields = mapObjectToList(template.fields, 'id').map(f => {
+    //   const { mock, ...all } = f
+    //   return all
+    // })
 
-    if (_.size(template.refs)) {
-      template.refs = mapObjectToList(template.refs, 'id')
-    }
+    // if (_.size(template.refs)) {
+    //   template.refs = mapObjectToList(template.refs, 'id')
+    // }
 
     return template
   })
